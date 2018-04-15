0f4aeea4a6a0145cccc1004f58dc87272bb87cce
Maciej Caputa <kontakt@maciejcaputa.pl>
2018-04-15 03:44:44 +0200

    Add navbar

--- maciejcaputablog/Web/wwwroot/js/site.js
+++ maciejcaputablog/Web/wwwroot/js/site.js
@@ -1,1 +1,24 @@
-﻿// Write your JavaScript code.
+﻿(function ($) {
+    $(document).ready(function () {
+
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+                var isMobile = window.matchMedia("(max-width: 767.98px)");
+
+                if (!isMobile.matches) {
+                    if ($(this).scrollTop() > 180) {
+                        $('.info-bar-properties').slideUp();
+                        $('.navabar-brand-properties').slideUp();
+
+                    }
+                    else {
+                        $('.info-bar-properties').slideDown();
+                        $('.navabar-brand-properties').slideDown();
+                    }
+                }
+            });
+        });
+
+    });
+}(jQuery));
